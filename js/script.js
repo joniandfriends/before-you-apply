b8646551a47705a25527ab9ec5d68df3530daed7
github username <github email address>
2022-03-24 19:20:07 -0400

    Completed dynamic button code.

--- js/script.js
+++ js/script.js
@@ -1,25 +1,37 @@
-// sample URL https://www.joniandfriends.org/before-you-apply/?fr_id=2197&s_partType=primary
+// sample URL coming from FR event page: https://www.joniandfriends.org/before-you-apply/?fr_id=2197&s_partType=primary
 // sample Query String ?fr_id=2197&s_partType=primary
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString)
 const retreatId = urlParams.get('fr_id')
 const participantType = urlParams.get('s_partType')
-
+const applyButton = document.getElementById('apply-btn')
 const groupText = document.getElementById('group-text')
 const individualText = document.getElementById('individual-text')
-console.log(groupText)
-console.log(individualText)
+const errorMessage = document.getElementById('error-message')
+const applyButtonUrl = `https://secure.joniandfriends.org/site/TRR/FamilyRetreats/General?pg=tfind&fr_id=${retreatId}&fr_tm_opt=new&s_partType=${participantType}`
 
-// const downloadText = document.getElementById('download-text')
-// const subscribeText = document.getElementById('subscribe-text')
-
-// if (formName === 'WhyBadThingsHappen') {
-//   downloadText.classList.remove('hide')
-//   defaultText.classList.add('hide')
-// } else if (formName === 'AMEmailOptin') {
-//   subscribeText.classList.remove('hide')
-//   defaultText.classList.add('hide')
-// } else {
-//   console.log("No match")
-// }
+switch(participantType){
+  case 'primary':
+    applyButton.href = applyButtonUrl
+    applyButton.classList.remove('hide')
+  break;
+  case 'volunteer':
+    groupText.classList.add('hide')
+    individualText.classList.remove('hide')
+    applyButton.href = applyButtonUrl
+    applyButton.classList.remove('hide')
+  break;
+  case 'volunteerGroup':
+    applyButton.href = applyButtonUrl
+    applyButton.classList.remove('hide')
+  break;
+  case 'pca':
+    applyButton.href = applyButtonUrl
+    applyButton.classList.remove('hide')
+  break;
+  default:
+    // We should never match this case!
+    errorMessage.classList.remove('hide')
+    applyButton.href = '/what-are-you-talking-about-willis'
+}
